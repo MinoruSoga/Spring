1a4bf3250dbb7b7e40a4666c0fedad06f972efb4
MinoruSoga <minoru.2021@icloud.com>
2019-08-07 14:49:27 +0900

    first commit

---
+++ LessonSpringMVC/src/main/java/jp_co/good_works/lesson/springmvc/controller/HelloController.java
@@ -0,0 +1,27 @@
+package jp_co.good_works.lesson.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+//このファイルはコントローラーです。という宣言　MVCモデルで今回作成しているので記載は必須。
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HelloController {
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	//helloとアクセスされたら　下記helloメソッドが実行される
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	//modelという箱にmessageという名の名前でHelloSpringという文字列を格納している
+	public String hello(Model model) {
+		
+		model.addAttribute("message", "Hello,Spring!" );
+		//hello.jspへ遷移する。
+		return "home";
+	}
+	
+}
