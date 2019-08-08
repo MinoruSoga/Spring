84204617a9705fe35383113d79f3ac8513c761a6
MinoruSoga <minoru.2021@icloud.com>
2019-08-08 11:27:10 +0900

    form

---
+++ LessonSpringMVC/src/main/java/jp_co/good_works/lesson/springmvc/controller/ProductController.java
@@ -0,0 +1,28 @@
+package jp_co.good_works.lesson.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp_co.good_works.lesson.springmvc.form.ProductForm;
+
+@Controller
+public class ProductController {
+	@RequestMapping(value = "/product", method = RequestMethod.GET)
+	public String product(Model model) {
+		ProductForm form = new ProductForm();
+		model.addAttribute("message", "商品情報を入力してください");
+		model.addAttribute("productForm", form);
+		return "product";
+	}
+	
+	@RequestMapping(value = "/product", method = RequestMethod.POST)
+	public String product(Model model, @ModelAttribute ProductForm form) {
+		model.addAttribute("message", "商品情報が入力されました");
+		model.addAttribute("productForm", form);
+		return "product_result";
+	}
+	
+}
