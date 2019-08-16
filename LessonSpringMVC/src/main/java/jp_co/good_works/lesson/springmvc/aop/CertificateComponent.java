9dbe2358d36b4c87bcc0b1b3b74c441991b6608a
MinoruSoga <minoru.2021@icloud.com>
2019-08-16 11:43:31 +0900

    final

--- LessonSpringMVC/src/main/java/jp_co/good_works/lesson/springmvc/aop/CertificateComponent.java
+++ LessonSpringMVC/src/main/java/jp_co/good_works/lesson/springmvc/aop/CertificateComponent.java
@@ -8,15 +8,21 @@ import org.springframework.web.servlet.support.WebContentGenerator;
 
 import jp_co.good_works.lesson.springmvc.controller.no_cert.LoginController;
 
-@Aspect
+@Aspect 
 @Component
 public class CertificateComponent extends WebContentGenerator{
-
+//	指定したディレクトリ配下のコントローラーにAOPを適用させる
     @Around("execution(* jp_co.good_works.lesson.springmvc.controller.HelloController.*(..))")
+    //　↑　メソッド実行前に呼ばれ、この中でメソッドを実行する/メソッド実行の代わりに呼び出す
+    
     public String checkAuthenticated(ProceedingJoinPoint joinPoint)throws Throwable{
+    	//ログインコントローラーをAOPとして指定している（クラスファイルを指定しなければならない）
+    	//インスタンス化と同等な扱いができます
         LoginController loginController = getApplicationContext().getBean(LoginController.class);
         if(loginController.isLive()){
+//        	指定したcontrollerに遷移する
             return (String) joinPoint.proceed();
+            // JoinPoint アドバイス（Advice:抽出した共通処理）を入れる場所
         }
         return "redirect:/login";
     }
